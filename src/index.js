5361cc430437945b1c93014f8ec971ffde3713d1
Roman Korotaev <kro1986@ukr.net>
2021-08-08 22:45:45 +0300

    Будем привязывать тему к атрибуту checked

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-//-------------- Импортируем сюда данніе из других файлов
-
-
+//-------------- Импортируем сюда данные из других файлов
 import './sass/main.scss';
 import product_itemTpl from './templates/product-item.hbs';
 import products from './menu.json';
 
-console.log('product_itemTpl = ', product_itemTpl(products[0]));
+
+//console.log('product_itemTpl = ', product_itemTpl(products[0]));
 
 const productsMenuRef = document.querySelector('ul.js-menu');
 const cardsMarkup = createProductsMenu(products);
@@ -32,14 +31,13 @@ const themeOfProductsMenuRef = document.querySelector('body');
 
 const checkboxRef = document.querySelector('#theme-switch-toggle');
 
+//console.log ('checkboxRef ', checkboxRef.getAttribute('checked'))
 
 
-themeOfProductsMenuRef.addEventListener('scrpll', changeTheme);
+if (checkboxRef.getAttribute('checked')!==null) {
+    console.log('It`s checked');
+}
 
-function changeTheme (event) {
-    event.preventDefault();
-    themeOfProductsMenuRef.classList.add('dark-theme');
-};
 
 
 
@@ -48,46 +46,15 @@ function changeTheme (event) {
 
 
 /*
-function createProductsMenu(products) {
-  const markup = products.map(({image, name, price, description, ingredients }) => {
-    return `
-    <li class="menu__item">
-    <article class="card">
-        <img src="{{image}}"
-            alt="{{name}}" class="card__image" />
-        <div class="card__content">
-            <h2 class="card__name">{{name}}</h2>
-            <p class="card__price">
-                <i class="material-icons"> monetization_on </i>
-                {{price}} кредитов
-            </p>
+themeOfProductsMenuRef.addEventListener('scrpll', changeTheme);
 
-            <p class="card__descr">
-                {{description}}
-            </p>
+function changeTheme (event) {
+    event.preventDefault();
+    themeOfProductsMenuRef.classList.add('dark-theme');
+};
 
-            <ul class="tag-list">
-                <li class="tag-list__item">{{ingredients}}</li>
-                <li class="tag-list__item">{{ingredients}}</li>
-                <li class="tag-list__item">{{ingredients}}</li>
-                <li class="tag-list__item">{{ingredients}}</li>
-                <li class="tag-list__item">{{ingredients}}</li>
-                <li class="tag-list__item">{{ingredients}}</li>
-                <li class="tag-list__item">{{ingredients}}</li>
-            </ul>
-        </div>
-
-        <button class="card__button button">
-            <i class="material-icons button__icon"> shopping_cart </i>
-            В корзину
-        </button>
-    </article>
-</li>
-  `;
-    
-  });
-  return markup.join('');
- }
+*/
 
 
- */
+
+
